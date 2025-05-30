f7e96cd3f7bfa6d6068932fd9b90eda4c26ba3e8
Oğuzhan Durgun <oguzhandurgun95@gmail.com>
2025-05-30 17:25:10 +0300

    Implement ChangeDetails, File, FileFilter, FileOp and StringMatch classes along with their unit tests
    Signed-off-by: Oğuzhan Durgun <oguzhandurgun95@gmail.com>

---
+++ src/Sdk/Cerbos/Sdk/Cloud/V1/Store/FileFilter.cs
@@ -0,0 +1,31 @@
+// Copyright 2021-2025 Zenauth Ltd.
+// SPDX-License-Identifier: Apache-2.0
+
+namespace Cerbos.Sdk.Cloud.V1.Store
+{
+    public sealed class FileFilter
+    {
+        private StringMatch Path { get; set; }
+
+        private FileFilter() { }
+
+        public static FileFilter NewInstance()
+        {
+            return new FileFilter();
+        }
+
+        public FileFilter WithPath(StringMatch path)
+        {
+            Path = path;
+            return this;
+        }
+
+        public Api.Cloud.V1.Store.FileFilter ToFileFilter()
+        {
+            return new Api.Cloud.V1.Store.FileFilter()
+            {
+                Path = Path.ToStringMatch()
+            };
+        }
+    }
+}
